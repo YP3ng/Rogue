ac0fac650e0d2663f7eee17cf648cd45f822f82c
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-04-29 01:35:15 +1000

    feat: added ItemClass.java

--- Entity.java
+++ Entity.java
@@ -1,3 +1,9 @@
+/**
+ * This abstract class is a base class for all entities in Rogue game.
+ * So far, we only have three derived classes, Player, Monster, ItemClass
+ * @author Yi-Cheng Peng, yicpeng@student.unimelb.edu.au, 1319296
+ *
+ */
 public abstract class Entity {
 
     public abstract void name();
