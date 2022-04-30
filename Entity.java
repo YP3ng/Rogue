d48cced60db1b797580cd25808c19205f366c367
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-04-30 17:45:00 +1000

    changed comments

--- Entity.java
+++ Entity.java
@@ -2,7 +2,7 @@
  * The world manages a lot entities within it. 
  * It would make sense for entities to know how to render themselves, 
  * and hold positional information about where they are in the world.
- * So far, we only have three derived classes, Player, Monster, ItemClass
+ * So far, we only have two derived classes, Unit, Item
  * @author Yi-Cheng Peng, yicpeng@student.unimelb.edu.au, 1319296
  *
  */
